a0d7c81a7a7856827ea56247b32fb7d7e52ad4aa
Ilyas Hassan <ilyashassan4400@gmail.com>
2021-06-06 19:46:41 -0400

    Update groceries.js

--- groceries.js
+++ groceries.js
@@ -109,7 +109,7 @@ function restrictListProducts(prods, restriction) {
 function getTotalPrice(chosenProducts) {
 	totalPrice = 0;
 	for (let i=0; i<products.length; i+=1) {
-		if (chosenProducts.indexOf(products[i].name) > -2){
+		if (chosenProducts.indexOf(products[i].name) > 0){
 			totalPrice += products[i].price;
 		}
 	}
