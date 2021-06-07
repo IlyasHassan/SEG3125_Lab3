43d771025c53fdbbb4cb07c9596fb6d5000e670c
Ilyas Hassan <ilyashassan4400@gmail.com>
2021-06-06 21:43:23 -0400

    Gradient

--- groceries.js
+++ groceries.js
@@ -109,7 +109,7 @@ function restrictListProducts(prods, restriction) {
 function getTotalPrice(chosenProducts) {
 	totalPrice = 0;
 	for (let i=0; i<products.length; i+=1) {
-		if (chosenProducts.indexOf(products[i].name) > 0){
+		if (chosenProducts.indexOf(products[i].name) > -1){
 		
 			totalPrice += products[i].price;
 		}
